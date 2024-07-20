1f06d66c0db4a6e871329f56483befc0bf9278ed
HusseinAlaa <hussein4alaa@gmail.com>
2024-07-20 10:58:14 +0300

    Update User.php

--- src/Models/User.php
+++ src/Models/User.php
@@ -5,13 +5,12 @@ namespace g4t\FilamentAccessControl\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 use Filament\Panel;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles;
 
     protected $guarded = [];
 
